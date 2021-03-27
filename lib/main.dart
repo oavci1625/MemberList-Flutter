8482b8d533b099573c4d690f7c5e69e64ff573f3
Onuralp AVCI <63224890+oavci1625@users.noreply.github.com>
2021-03-27 23:52:10 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -133,7 +133,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       backgroundColor: Colors.grey[100],
                     ),
                     onTap: (){
-                      print(filteredMembers.list[i].hipo.yearsInHipo);
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(builder: (BuildContext context) => new ProfilePage(filteredMembers.list[i], i)));
                     },
                   ),
                 );
@@ -153,12 +155,139 @@ class _MyHomePageState extends State<MyHomePage> {
             style: TextStyle(color: Colors.white),),
         ),
         onPressed: (){
-          Hipo myHipo = Hipo("Android developer", 0);
-          Member me = Member("Onuralp Avci", 20, "Bursa", "guthub/oavci", myHipo);
+          Hipo myHipo = Hipo("Intern", 0);
+          Member me = Member("Onuralp Avci", 20, "Bursa", "oavci1625", myHipo);
           add(me);
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+}
+
+class ProfilePage extends StatelessWidget{
+  List<AssetImage> imageList = [AssetImage("assets/avatar1.png"), AssetImage("assets/avatar2.png"), AssetImage("assets/avatar3.png"), AssetImage("assets/avatar4.png")];
+  ProfilePage(this.data, this.index);
+  final data;
+  final index;
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+    appBar: new AppBar(
+      title: new Text("Profile - " + data.name)
+    ),
+    body:
+      new Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50.0,
+              ),
+              new Container(
+                width: 150.0,
+                height: 150.0,
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(image: imageList[index%4]),
+                  borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
+                  border: new Border.all(
+                    color: Colors.red,
+                    width: 4.0,
+                  )
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(data.name,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              Text(data.age.toString(),
+                style: TextStyle(
+                  fontSize: 15.0,
+                ),
+              ),
+              SizedBox(
+                child: Divider(
+                  color: Colors.red,
+                  thickness: 5,
+                ),
+                height: 40.0,
+              ),
+              Row(
+                children: [
+                  Text('Location:          ',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(data.location,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: [
+                  Text('Github:              ',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(data.github,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: [
+                  Text('Position:            ',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(data.hipo.position,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: [
+                  Text('Years in hipo:   ',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(data.hipo.yearsInHipo.toString(),
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      )
+  );
 }
